10085f15a3899a62ff91ff71e8ebca9dd3bc8864
Abel Segui <Abelsegui@hotmail.com>
2020-05-02 13:37:24 +0000

    done

--- src/js/component/TraficLights.js
+++ src/js/component/TraficLights.js
@@ -9,22 +9,31 @@ class TraficLights extends Component {
 	}
 	render() {
 		console.log(this.state);
+		var redExtraClass = "";
+		if (this.state.clickedLight == "red") redExtraClass = "selected";
+
+		var yellowExtraClass = "";
+		if (this.state.clickedLight == "yellow") yellowExtraClass = "selected";
+
+		var greenExtraClass = "";
+		if (this.state.clickedLight == "green") greenExtraClass = "selected";
+
 		return (
-			<div className="content">
+			<div className="content ">
 				<div className="trafic-top" />
 				<div className="container">
 					<div
-						className="red-light "
+						className={"red light " + redExtraClass}
 						onClick={() => this.setState({ clickedLight: "red" })}
 					/>
 					<div
-						className="yellow-light"
+						className={"yellow light " + yellowExtraClass}
 						onClick={() =>
 							this.setState({ clickedLight: "yellow" })
 						}
 					/>
 					<div
-						className="green-light"
+						className={"green light " + greenExtraClass}
 						onClick={() => this.setState({ clickedLight: "green" })}
 					/>
 				</div>
